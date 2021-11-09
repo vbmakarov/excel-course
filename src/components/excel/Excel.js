efb2fb307e2a004b08ec61d1921536e9b25e230d
vbmakarov <38083680+vbmakarov@users.noreply.github.com>
2021-11-09 18:20:58 +0300

    Framework start (#3)
    * Finish base framework functional
    
    * comment debug

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,30 @@
+import {$} from '@core/dom';
+
+export class Excel {
+  constructor(selector, options) {
+    this.$sel = document.querySelector(selector)
+    this.components = options.components || []
+  }
+
+  getRoot() {
+    const $root = $.create('div', 'Excel')
+    this.components = this.components.map((Component) => {
+      const elem = $.create('div', Component.className)
+      const component = new Component(elem)
+      // DEBUG
+      // if (component.name) {
+      //   console.log(component.name)
+      //   window['c' + component.name] = component
+      // }
+      elem.html(component.toHtml())
+      $root.append(elem)
+      return component;
+    })
+    return $root
+  }
+
+  render() {
+    this.$sel.append(this.getRoot().$elem)
+    this.components.forEach((component) => component.init())
+  }
+}
